203a7753924c5b2d5cdf0ed648a37f6f4771762b
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-20 15:51:50 +0100

    .

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+declare module "@styles/hljs/1c-light" {
+  const css: void;
+  export default css;
+}
+
+// declare module "@styles/hljs/atom-one-dark" {
+//   const css: void;
+//   export default css;
+// }
+
+// // ...add more as needed
---
+++ test/index.js
@@ -0,0 +1,1 @@
+import "@styles/hljs/1c-light"
